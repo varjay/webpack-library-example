34bce734210041659a3dee56d5fed8aa4dc0ded2
goudan <goudan@baidu.com>
2018-08-15 15:42:26 +0800

    更新babel

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,33 +1,36 @@
 const path = require('path');
 
-export default () => (
-    {
-        mode: 'production',
-        entry: './index.js',
-        output: {
-            path: path.resolve(__dirname, './dist'),
-            filename: 'webpack-numbers.js',
-            libraryTarget: 'umd',
-            globalObject: 'this',
-            // libraryExport: 'default',
-            library: 'webpackNumbers'
-        },
-        externals: {
-            'lodash': {
-                commonjs: 'lodash',
-                commonjs2: 'lodash',
-                amd: 'lodash',
-                root: '_'
-            }
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js)$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: 'babel-loader'
+module.exports = {
+    mode: 'production',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'webpack-numbers.js',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        // libraryExport: 'default',
+        library: 'webpackNumbers'
+    },
+    externals: {
+        'lodash': {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
+    },
+    module: {
+        rules: [
+            {
+              test: /\.js$/,
+              exclude: /(node_modules|bower_components)/,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env']
                 }
-            ]
-        },
-    }
-);
+              }
+            }
+        ]
+    },
+}
